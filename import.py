5eb5078dfe38269fa1120a8d00a37c4fa6cf72ea
ErwanVP <erwan.vanpeteghem@qxlricardo.fr>
2016-09-14 11:17:51 +0200

    Update import.py

--- import.py
+++ import.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-import json
 import urllib
 import boto3
 
@@ -9,9 +8,7 @@ print('Loading function')
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
 
-    # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
     try:
