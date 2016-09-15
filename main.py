7aaea131385210ba76aab7dac574ec30db397580
ErwanVP <erwan.vanpeteghem@qxlricardo.fr>
2016-09-15 10:29:30 +0200

    amter main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import urllib2
+
 import boto3
 
 print('Loading function')
@@ -10,7 +10,7 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
 
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+    key = event['Records'][0]['s3']['object']['key']
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
         print("CONTENT TYPE: " + response['ContentType'])
@@ -18,4 +18,4 @@ def lambda_handler(event, context):
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-raise e
+        raise e
